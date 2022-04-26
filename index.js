2b4fd9bbf2927d7954c6a671e2d2710ab553334a
Daniel <danchan020@gmail.com>
2022-04-25 22:50:53 -0700

    commit tree 386ac112fd902f915d4d1eba500956a75b6a8ac3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+function multiply(num1,num2){
+    return num1 * num2};
+    let results = multiply(31,2); 
+    console.log(results)
 
-// Write your code here
+function random(){return (Math.random() * Infinity) + 1}
+
+function mod(num3,num4){
+    return num3 % num4};
+    let remainder = mod(20,8);
+    console.log(remainder)
